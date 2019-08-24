c39a1cfa9835f470881bec5b1f38fae94ca000d7
Omar Bishtawi <omar@dawsat.com>
2019-08-24 14:20:14 +0300

    add firebase configraion

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -5,13 +5,15 @@
 import firebase from 'firebase/app'
 
 // Initialize Firebase
+//TODO add to git ignore
 var config = {
-    apiKey: "AIzaSyDFYIpJlgGZBHyjcaiIKgPAadzSofuYaCY",
-    authDomain: "vuesax-admin.firebaseapp.com",
-    databaseURL: "https://vuesax-admin.firebaseio.com",
-    projectId: "vuesax-admin",
-    storageBucket: "vuesax-admin.appspot.com",
-    messagingSenderId: "914001522995"
+    apiKey: "AIzaSyA_fo7aX8Yp2AK2xPkW1tIfOM3_1XNPnps",
+    authDomain: "tlap-e9cd6.firebaseapp.com",
+    databaseURL: "https://tlap-e9cd6.firebaseio.com",
+    projectId: "tlap-e9cd6",
+    storageBucket: "tlap-e9cd6.appspot.com",
+    messagingSenderId: "259568810278",
+    appId: "1:259568810278:web:1a9bd6ac6be43a4a"
 };
 
 firebase.initializeApp(config);
