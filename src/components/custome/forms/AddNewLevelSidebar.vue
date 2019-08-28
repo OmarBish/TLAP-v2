7d9b8471c80d7974c71837e41190fe3f89c93092
Omar Bishtawi <omar@dawsat.com>
2019-08-29 01:12:11 +0300

    Contest levels mangment

--- src/components/custome/forms/AddNewLevelSidebar.vue
+++ src/components/custome/forms/AddNewLevelSidebar.vue
@@ -11,7 +11,7 @@
 <template>
   <vs-sidebar click-not-close position-right parent="body" default-index="1" color="primary" class="add-new-data-sidebar items-no-padding" spacer v-model="isSidebarActiveLocal">
     <div class="mt-6 flex items-center justify-between px-6">
-        <h4>ADD NEW DATA</h4>
+        <h4>ADD NEW Level</h4>
         <feather-icon icon="XIcon" @click.stop="isSidebarActiveLocal = false" class="cursor-pointer"></feather-icon>
     </div>
     <vs-divider class="mb-0"></vs-divider>
@@ -22,26 +22,26 @@
         <!-- NAME -->
         <vs-input label="Name" name="name" v-model="name" class="mt-5 w-full" />
 
-        <!-- CATEGORY -->
-        <vs-select v-model="category" label="Level" class="mt-5 w-full">
-          <vs-select-item :key="item.value" :value="item.value" :text="item.text" v-for="item in category_choices" />
-        </vs-select>
+        <div style="display:flex;justify-content:space-around">
+          <!-- Difficulty -->
+          <span class="mt-5" style="display: inline-block;">
+              <label class="vs-input--label ">Difficulty</label>
+              <vs-input-number v-model="difficulty"/>
+            </span>
+            <!-- Score -->
+            <span class="mt-5" style="display: inline-block;">
+              <label class="vs-input--label ">Score</label>
+              <vs-input-number v-model="score"/>
+            </span>
+        </div>
+          
 
-        <!-- ORDER STATUS -->
-        <vs-select v-model="order_status" label="Question Status" class="mt-5 w-full">
-          <vs-select-item :key="item.value" :value="item.value" :text="item.text" v-for="item in order_status_choices" />
-        </vs-select>
-
-        <!-- PRICE -->
-        <!-- <vs-input label="Price" name="price" v-model="price" class="mt-5 w-full" /> -->
-
-        <!-- IMG -->
-        <vs-upload text="Upload Image" class="img-upload" ref="fileUpload" />
+        
       </div>
     </VuePerfectScrollbar>
 
     <div class="flex flex-wrap items-center justify-center p-6" slot="footer">
-      <vs-button class="mr-6">Add Data</vs-button>
+      <vs-button class="mr-6" @click="submitContest">Add Contest</vs-button>
       <vs-button type="border" color="danger" @click="isSidebarActiveLocal = false">Cancel</vs-button>
     </div>
   </vs-sidebar>
@@ -60,21 +60,8 @@ export default {
   data() {
     return {
       name: '',
-      category: 'Easy',
-      order_status: 'pending',
-      price: '',
-
-      category_choices: [
-        {text:'Easy',value:'Easy'},
-        {text:'Medium',value:'Medium'},
-        {text:'Hard',value:'Hard'},
-      ],
-      order_status_choices: [
-        {text:'Pending',value:'pending'},
-        {text:'Canceled',value:'canceled'},
-        {text:'Delivered',value:'delivered'},
-        {text:'On Hold',value:'on_hold'}
-      ],
+      difficulty: 1,
+      score:15,
       settings: { // perfectscrollbar settings
           maxScrollbarLength: 60,
           wheelSpeed: .60,
@@ -89,18 +76,22 @@ export default {
       set(val) {
         if(!val) {
           this.$emit('closeSidebar');
-          this.initValues();
         }
       }
     }
   },
   methods: {
-    initValues() {
-      this.name = '';
-      this.category = 'audio';
-      this.order_status = 'pending';
-      this.price = '';
-      this.$refs.fileUpload.srcs = [];
+    submitContest(){
+        const payload={
+          level:{
+            name:this.name,
+            difficulty:this.difficulty,
+            score:this.score,
+          },
+          notify:this.$vs.notify
+        }
+        this.$store.dispatch('levels/addLevel',payload)
+        this.isSidebarActiveLocal = false
     }
   },
   components: {
--- src/views/Questions.vue
+++ src/views/Questions.vue
@@ -1,43 +1,93 @@
 <template>
-  <div class="mt-5">
-    <h1 class="mb-5 text-center">Levels</h1>
-    <vs-tabs alignment="fixed">
-      <vs-tab label="Easy">
+<div>
+  <add-new-level-sidebar :isSidebarActive="addNewLevelSidebar" @closeSidebar="addNewLevelSidebar = false" />
+  <div class="mt-5" v-if="contestEnded">
+    <div class="add-new-level">
+      <h1 class="mb-5 mr-2 text-center">
+        {{currentContestName}}
+      </h1> 
+      <vx-tooltip text="Add new level">
+        <vs-button @click="addNewLevelSidebar = true" class="cursor-pointer" radius icon="icon-plus" icon-pack="feather"></vs-button>
+      </vx-tooltip>
+    </div>
+    <div v-if="levels.length == 0">
+      <h3 class="text-center">
+        There isn't any level added to this contest
+      </h3>
+    </div>
+    <vs-tabs alignment="fixed" v-if="levels.length > 0">
+      <vs-tab v-for="(level,levelIndex) in levels" :key="levelIndex" :data="level" :label="level.name">
         <div>
-			<questions></questions>
-		</div>
-      </vs-tab>
-      <vs-tab label="Medium">
-        <div>
-			<questions></questions>
-		</div>
-      </vs-tab>
-      <vs-tab label="Hard">
-        <div>
-			<questions></questions>
-		</div>
-      </vs-tab>
-      <vs-tab label="Expert">
-        <div>
-			<questions></questions>
-		</div>
-      </vs-tab>
-	  <vs-tab label="Hell">
-        <div>
-			<questions></questions>
-		</div>
+          <questions></questions>
+        </div>
       </vs-tab>
     </vs-tabs>
   </div>
+  <div v-if="!contestEnded">
+    <h1 class="text-center">
+      You haven't particepated in any contest
+    </h1>
+  </div>
+</div>
+  
 </template>
 
 <script>
 import Questions from "../components/custome/QuestionsView.vue"
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+import AddNewLevelSidebar from '../components/custome/forms/AddNewLevelSidebar';
+
 export default {
+  data(){
+    return {
+      addNewLevelSidebar:false
+    }
+  },
+  computed:{
+    levels(){
+      return this.$store.getters['levels/getLevels']
+    },
+    currentContestName(){
+      return JSON.parse(localStorage.getItem('userInfo')).current_contest.name
+    },
+    contestEnded(){
+      let currentContest = JSON.parse(localStorage.getItem('userInfo')).current_contest
+      let startTime = currentContest.start_time.seconds
+      console.log('startTime',startTime)
+      let duration = currentContest.duration
+      console.log('duration',duration)
+      let endtime = startTime + duration
+      console.log('endtime',endtime)
+      let currentTime = firebase.firestore.Timestamp.now().seconds
+      console.log('currentTime',currentTime)
+      console.log('endtime < currentTime',endtime < currentTime)
+      if( endtime > currentTime){
+          return true
+      }else{
+        return false
+      }
+    }
+  },
+  created(){
+      this.$store.dispatch('levels/fetchLevels')
+  },
 	components:{
-		Questions
+    Questions,
+    AddNewLevelSidebar
 	}
-
 }
-
 </script>
+<style >
+.add-button{
+  position: fixed;
+  bottom: 10px;
+  right:10px;
+  z-index: 9999;
+}
+.add-new-level{
+  display: flex;
+  justify-content: center;
+}
+</style>
